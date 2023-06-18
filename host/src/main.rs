2e1d770105d21c8886f3b28b000c1c723e5c41de
mysteryfigure <mysteryfigure@gmail.com>
2023-06-18 11:49:32 -0400

    run set_commit without increments

--- host/src/main.rs
+++ host/src/main.rs
@@ -18,7 +18,7 @@ fn analyze() {
 
     println!("Reading metamath file");
 
-    let axiom_file = File::open("theory/set_comp5.mm".clone()).expect("Failed to find file"); // mm-benchmarks/transfer-goal.mm
+    let axiom_file = File::open("theory/set_comp.mm".clone()).expect("Failed to find file"); // theory/mm-benchmarks/transfer-goal.mm theory/set_comp5.mm
 
     let axiom_file_lines: Vec<String> = BufReader::new(axiom_file)
         .lines()
@@ -27,22 +27,105 @@ fn analyze() {
 
     println!("Metamath file has {} lines.", axiom_file_lines.len());
 
-    let mut max_lines = 10;
-    while max_lines < axiom_file_lines.len() {
+    // let mut max_lines = 10;
+    // while max_lines < axiom_file_lines.len() {
 
-        max_lines *= 2;
+    //     max_lines *= 2;
 
-        let num_lines_to_take = cmp::min(max_lines, axiom_file_lines.len());
+    //     let num_lines_to_take = cmp::min(max_lines, axiom_file_lines.len());
 
-        // let mut shortened_axiom_file_lines = axiom_file_lines.copy();
+    //     // let mut shortened_axiom_file_lines = axiom_file_lines.copy();
 
-        // for line in &axiom_file_lines[0..num_lines_to_take] {
-        //     println!("{}", line);
-        // }
+    //     // for line in &axiom_file_lines[0..num_lines_to_take] {
+    //     //     println!("{}", line);
+    //     // }
+
+    //     println!("Serializing metamath file");
+
+    //     let serialized_file: Vec<u32> = to_vec(&axiom_file_lines[0..num_lines_to_take]).unwrap();
+
+    //     println!("Serializing target theorem");
+
+    //     let target_theorem: String = "th1".to_string();
+
+    //     let serialized_target_theorem: Vec<u32> = to_vec(&target_theorem).unwrap();
+
+    //     // First, we construct an executor environment
+    //     // let env = ExecutorEnv::builder().build();
+
+    //     // TODO: add guest input to the executor environment using
+    //     // ExecutorEnvBuilder::add_input().
+    //     // To access this method, you'll need to use the alternate construction
+    //     // ExecutorEnv::builder(), which creates an ExecutorEnvBuilder. When you're
+    //     // done adding input, call ExecutorEnvBuilder::build().
+
+    //     println!("Creating environment");
+
+    //     // Default is const DEFAULT_SESSION_LIMIT: usize = 64 * 1024 * 1024; // 64M cycles
+
+    //     // For example: let env = ExecutorEnv::builder().add_input(&vec).build();
+    //     let env = ExecutorEnv::builder()
+    //         .session_limit(1024 * 1024 * 1024)
+    //         .add_input(&serialized_target_theorem)
+    //         .add_input(&serialized_file)
+    //         .build();
+
+    //     // Next, we make an executor, loading the (renamed) ELF binary.
+    //     let mut exec = Executor::from_elf(env, METHOD_NAME_ELF).unwrap();
+
+    //     println!("Running session");
+    //     let mut start_time = SystemTime::now();
+
+    //     // Run the executor to produce a session.
+    //     let session = exec.run().unwrap();
+
+    //     match SystemTime::now().duration_since(start_time) {
+    //         Ok(n) => println!(
+    //             "The non-ZK metamath ran the session, taking {} milliseconds for {} lines",
+    //             n.as_millis(),
+    //             num_lines_to_take
+    //         ),
+    //         Err(_) => panic!("SystemTime error"),
+    //     }
+
+    //     println!("Proving session");
+    //     start_time = SystemTime::now();
+
+    //     // Prove the session to produce a receipt.
+    //     let receipt = session.prove().unwrap();
+
+    //     match SystemTime::now().duration_since(start_time) {
+    //         Ok(n) => println!(
+    //             "The ZK metamath ran the session, taking {} milliseconds for {} lines",
+    //             n.as_millis(),
+    //             num_lines_to_take
+    //         ),
+    //         Err(_) => panic!("SystemTime error"),
+    //     }
+
+    //     println!("Verifying");
+    //     start_time = SystemTime::now();
+
+
+    //     let theorem_hash : Digest = from_slice(&receipt.journal).unwrap();
+    //     receipt.verify(METHOD_NAME_ID).unwrap();
+
+    //     match SystemTime::now().duration_since(start_time) {
+    //         Ok(n) => println!(
+    //             "The ZK metamath verifier succeeds in verifying the proof, taking {} milliseconds for {} lines",
+    //             n.as_millis(),
+    //             num_lines_to_take
+    //         ),
+    //         Err(_) => panic!("SystemTime error"),
+    //     }
+
+
+
+    // }
 
         println!("Serializing metamath file");
 
-        let serialized_file: Vec<u32> = to_vec(&axiom_file_lines[0..num_lines_to_take]).unwrap();
+        let serialized_file: Vec<u32> = to_vec(&axiom_file_lines).unwrap();
 
         println!("Serializing target theorem");
 
@@ -106,7 +189,6 @@ fn analyze() {
         println!("Verifying");
         start_time = SystemTime::now();
 
-
         let theorem_hash : Digest = from_slice(&receipt.journal).unwrap();
         receipt.verify(METHOD_NAME_ID).unwrap();
 
@@ -118,11 +200,6 @@ fn analyze() {
             ),
             Err(_) => panic!("SystemTime error"),
         }
-
-
-
-    }
-
 }
 
 fn main() {
